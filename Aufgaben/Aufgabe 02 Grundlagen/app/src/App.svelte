fbba0c58a7f55f9ed72b33a764eb8e54f00a21c3
User01name <02goto1bif@hft-stuttgart.de>
2024-04-30 19:31:06 +0200

    Update Structure - remove unnecessary folders and README

--- Aufgaben/Aufgabe 02 Grundlagen/app/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
- let src = "images/image.png"
- // add variables here
-</script>
-
-
-<main>
-    <!-- add h1 and img here -->
-</main>
---
+++ Aufgaben/Aufgabe 02 Grundlagen/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  let src = "images/image.png"
+  // add variables here
+ </script>
+ 
+ 
+ <main>
+     <!-- add h1 and img here -->
+ </main>
+ 
