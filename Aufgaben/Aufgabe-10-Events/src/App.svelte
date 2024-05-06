db8dbc004b7e42305e2f2bfdb3352e19fa3376af
TobiasHeimGalindo <tobisellfy@gmail.com>
2024-05-06 16:51:11 +0200

    fix: typo

--- Aufgaben/Aufgabe-10-Events/src/App.svelte
+++ Aufgaben/Aufgabe-10-Events/src/App.svelte
@@ -16,7 +16,7 @@
 
   function handleAddPizza(event) {
     const { topping } = event.detail;
-    // verhindere doppelte Toppings
+    // verhindert doppelte Toppings
     if (cart.some((item) => item.id === topping.id)) return;
 
     // Aufgabe 2: Handle das addTopping Event hier
