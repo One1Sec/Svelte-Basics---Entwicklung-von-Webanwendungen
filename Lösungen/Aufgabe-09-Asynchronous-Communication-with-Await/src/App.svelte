621dfe6e7b8f36801b79629a31047913993a54b4
User01name <02goto1bif@hft-stuttgart.de>
2024-05-08 00:43:47 +0200

    Add solution for task 2 for Aufgabe 09

--- Lösungen/Aufgabe-09-Asynchronous-Communication-with-Await/src/App.svelte
+++ Lösungen/Aufgabe-09-Asynchronous-Communication-with-Await/src/App.svelte
@@ -21,7 +21,41 @@
 
 </main>
 
+  <!-- Mögliche Lösung von Aufgabe 2: -->
+  <!--
+    <script>
+      import { processOrder } from "./lib/utils";
+      let promise;
+      let buttonEnabled = true
+    </script>
+
+
+    <main>
+      <h1>Mario & Luigi's Pizza</h1>
+
+      <button on:click = {() => {if(buttonEnabled){
+          promise = processOrder()
+          buttonEnabled = false
+        }}}>
+        Process Order
+      </button>
+      {#if promise!=undefined}
+        {#await promise}
+          <p>Processing...</p>
+        {:then promise}
+          <p> {promise} </p>
+        {:catch error}
+          <p id="error_msg">{error.message}</p>
+          <img on:error={() => buttonEnabled = true} class="hidden" src="definetly not a correct url" />
+        {/await}
+      {/if}
+    </main>
+  -->
+
 <style>
+  .hidden{
+    visibility: hidden;
+  }
   main {
     display: flex;
     justify-content: center;
