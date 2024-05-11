bf550a9d436c3e36b80cb4d4ac42ff24ab76dfd9
User01name <02goto1bif@hft-stuttgart.de>
2024-05-11 16:10:48 +0200

    Update Aufgabe 11 Add solution for Aufgabe 13

--- Lösungen/Aufgabe-11-Bindings/src/App.svelte
+++ Lösungen/Aufgabe-11-Bindings/src/App.svelte
@@ -1,21 +1,24 @@
 <script>
+  let description =
+    "Mario & Luigi's pizza service only uses <strong>fresh and local</strong> products 🍕🥦";
+  const src = "images/image.png";
+  const alt = "Mario and Luigi with a Pizza";
+  const goombaPizza = "Goomba's Special";
+  const yoshiPizza = "Joshi's Veggi";
 
-  let description = "Mario & Luigi's pizza service only uses <strong>fresh and local</strong> products 🍕🥦";
-  const src = "images/image.png"
-  const alt = "Mario and Luigi with a Pizza"
-  const goombaPizza = "Goomba's Special"
-  const yoshiPizza = "Joshi's Veggi"
+  let address = { street: "", number: "", city: "", plz: "" };
+  let numberOfGoombaPizzas = 0;
+  let numberOfJoshiPizzas = 0;
 
-  let address = {street:"",number:"", city:"", plz:""}
-  let numberOfGoombaPizzas = 0
-  let numberOfJoshiPizzas = 0
- 
+  let payment_methods = [
+    "Payment by direct debit",
+    "Paypal",
+    "Credit/Debit card",
+  ];
+  let selected_payment_method = "";
 
-  let payment_methods = ["Payment by direct debit", "Paypal", "Credit/Debit card"]
-  let selected_payment_method = ""
-
-  function submitRequest (){
-    alert("Your order has been submitted. It will be deliverd to you shortly")
+  function submitRequest() {
+    alert("Your order has been submitted. It will be deliverd to you shortly");
   }
 </script>
 
@@ -23,39 +26,72 @@
   <div id="header">
     <h1>Mario & Luigi's Pizza</h1>
     <p>{@html description}</p>
-    <img id="logo" {src} {alt}/>
+    <img id="logo" {src} {alt} />
   </div>
 
   <div class="form">
     <h2>Your Order</h2>
     <label>
-      <p> Goomba's Special </p>
+      <p>{goombaPizza}</p>
       <input type="number" bind:value={numberOfGoombaPizzas} min="0" max="10" />
       <input type="range" bind:value={numberOfGoombaPizzas} min="0" max="10" />
     </label>
     <label>
-      <p> Joshi's Veggi </p>
+      <p>{yoshiPizza}</p>
       <input type="number" bind:value={numberOfJoshiPizzas} min="0" max="10" />
       <input type="range" bind:value={numberOfJoshiPizzas} min="0" max="10" />
     </label>
     <h2>Address</h2>
-    <input class="textfield" bind:value={address.street} maxlength=100 placeholder="Street"/>
-    <input class="textfield" bind:value={address.number} maxlength=20 placeholder="Number"/>
-    <input class="textfield" bind:value={address.city} maxlength=100 placeholder="City"/>
-    <input class="textfield" bind:value={address.plz} maxlength=20 placeholder="PLZ"/>
-    <p> <strong>Your addess is: </strong> <br/> {address.street} {address.number} <br/> {address.plz} {address.city}</p>
+    <input
+      class="textfield"
+      bind:value={address.street}
+      maxlength="100"
+      placeholder="Street"
+    />
+    <input
+      class="textfield"
+      bind:value={address.number}
+      maxlength="20"
+      placeholder="Number"
+    />
+    <input
+      class="textfield"
+      bind:value={address.city}
+      maxlength="100"
+      placeholder="City"
+    />
+    <input
+      class="textfield"
+      bind:value={address.plz}
+      maxlength="20"
+      placeholder="PLZ"
+    />
+    <p>
+      <strong>Your addess is: </strong> <br />
+      {address.street}
+      {address.number} <br />
+      {address.plz}
+      {address.city}
+    </p>
     <h2>Payment method</h2>
     {#each payment_methods as method}
       <label>
-        <input type="radio" name="scoops" value={method} bind:group={selected_payment_method}/>
+        <input
+          type="radio"
+          name="scoops"
+          value={method}
+          bind:group={selected_payment_method}
+        />
         {method}
       </label>
     {/each}
-    <p> <strong>Your preferred payment method:</strong> {selected_payment_method}</p>
+    <p>
+      <strong>Your preferred payment method:</strong>
+      {selected_payment_method}
+    </p>
     <button on:click={submitRequest}> Submit your request </button>
   </div>
 </main>
-
 
 <style>
   #logo {
@@ -85,15 +121,15 @@
     background-color: #c40c0c;
   }
   .form {
-    height:100%;
+    height: 100%;
     flex: 1 1 auto;
     margin-left: auto;
     margin-right: auto;
-    display:block;
+    display: block;
     align-items: center;
   }
   .textfield {
-    display:block;
+    display: block;
     margin-left: auto;
     margin-right: auto;
     margin-block: 10px;
@@ -101,9 +137,8 @@
   }
 
   label {
-    display:block;
+    display: block;
     margin-left: auto;
     margin-right: auto;
   }
-
 </style>
---
+++ Lösungen/Aufgabe-13-Writable-Stores/src/lib/OrderSummary.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { pizzaGoombaCount } from "./stores";
+    import { pizzaYoshiCount } from "./stores";
+
+    import { yoshiPizza } from "./stores";
+    import {goombaPizza} from "./stores"
+
+    let pizzaGoombasSpecialCount
+
+    pizzaGoombaCount.subscribe((value) => {
+		pizzaGoombasSpecialCount = value;
+	});
+
+</script>
+
+<div>
+    {#if pizzaGoombasSpecialCount > 0}
+    <p>{pizzaGoombasSpecialCount} * {goombaPizza}</p>
+    {/if}
+    {#if $pizzaYoshiCount > 0}
+    <p>{$pizzaYoshiCount} * {yoshiPizza}</p>
+    {/if}
+</div>
+
+<style>
+
+</style>
